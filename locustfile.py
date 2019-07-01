dfdc4337164f7d210b081d4448a146a35c12f10d
Gabe Alzate <ggalzate98@gmail.com>
2019-07-01 23:10:54 +0800

    Ran test

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,17 @@
 from locust import HttpLocust, TaskSet, task
-import random
+import random, time
 
 class MyTaskSet(TaskSet):
+
+    names = open("names.txt").readlines()
+    ids = list(range(10000000, 100000000))
+    db = []
+    dorms = ["'Harwood'", "'Wig'", "'Oldenborg'", "'Lyon'", "'Smiley'",
+                    "'Mudd'", "'Blaisdell'", "'Sontag'", "'Dialynas'",
+                    "'Walker'", "'Clark I'", "'Clark III'"]
+
+    def on_start(self):
+        self.register()
 
     @task
     def list(self):
@@ -9,52 +19,59 @@ class MyTaskSet(TaskSet):
 
     @task
     def get(self):
-        if self.added:
-            self.client.get(f"/students/{self.added[-1]}")
-            added.insert(0, added.pop())
+        if self.db:
+            id = self.db[-1]
+            self.client.get(f"/students/{id}")
 
-    @task
+    @task(2)
     def register(self):
-        if self.ids
-            id = self.ids.pop()
-            name = "'" + random.choice(self.names) + " " + random.choice(self.names) + "'"
+        if self.ids:
+            first = random.choice(self.names).strip("\n")
+            last = random.choice(self.names).strip("\n")
+            name = "'" + first + " " + last + "'"
+
             dorm = random.choice(self.dorms)
-            self.client.post("/students/register", {"id": id, "name": name, "dorm": dorm })
-            self.added.append(id)
+            try:
+                id = self.ids[-1]
+                self.client.post("/students/register", json = {
+                        "id": id,
+                        "name": name,
+                        "dorm": dorm
+                })
+            except:
+                print(f"POST failed. Putting back {id}...")
+            else:
+                self.db.append(self.ids.pop())
+
 
     @task
     def delete(self):
-        if self.added:
-            self.client.delete(f"/students/delete/{self.added[-1]}")
-            ids.insert(0, added.pop())
+        if self.db:
+            id = self.db[-1]
+            self.client.delete(f"/students/delete/{id}")
+            self.ids.append(self.db.pop())
 
     @task
     def update(self):
-        if self.added:
-            choice = random.getrandbits(1)
-            id = self.added[-1]
+        if len(self.db):
+            id = self.db[-1]
+            changeDorm = random.getrandbits(1)
+            new = random.choice(self.dorms)
+            if not changeDorm:
+                first = random.choice(self.names).strip("\n")
+                last = random.choice(self.names).strip("\n")
+                new = "'" + first + " " + last + "'"
 
-            first = random.choice(self.names).strip("\n")
-            last = random.choice(self.names).strip("\n")
-            new = "'" + first + " " + last + "'"
-
-            if choice:
-                new = random.choice(self.dorms)
-            self.client.put(f"/students/update/{id}", {"changeDorm": choice, "new": new})
-            added.insert(0, added.pop())
-
+            try:
+                self.client.put(f"/students/update/{id}", json = {
+                        "changeDorm": changeDorm,
+                        "new": new
+                })
+            except:
+                print(f"PUT failed.... Update cancelled")
 
     def on_stop(self):
         self.client.delete("/students/clear")
-    
-    def __init__(self, names, ids, added):
-        self.ids = list(range(10000000, 100000000))
-        self.names = open("names.txt").readlines()
-        self.dorms = ["'Harwood'", "'Wig'", "'Oldenborg'", "'Lyon'", "'Smiley'",
-                        "'Mudd'", "'Blaisdell'", "'Sontag'", "'Dialynas'",
-                        "'Walker'", "'Clark I'", "'Clark III'"]
-        self.added = []
-
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
