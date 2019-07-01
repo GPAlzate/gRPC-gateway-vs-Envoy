2d9216019618d0494d56563fc3d292acf6de5505
Gabe Alzate <ggalzate98@gmail.com>
2019-07-01 15:50:49 +0800

    Finished test locust file

--- locustfile.py
+++ locustfile.py
@@ -3,30 +3,46 @@ import random
 
 class MyTaskSet(TaskSet):
 
+    @task
     def list(self):
-        name = "'" + random.choice(names) + " " + random.choice(names) + "'"
         self.client.get("/students")
 
     @task
     def get(self):
-        if len(list):
-            self.client.get(f"/students/{added[-1]}")
+        if self.added:
+            self.client.get(f"/students/{self.added[-1]}")
             added.insert(0, added.pop())
 
     @task
     def register(self):
-        id = self.ids.pop()
-        name = "'" + random.choice(self.names) + " " + random.choice(self.names) + "'"
-        dorm = random.choice(self.dorms)
-        self.client.post("/students/register", {"id": id, "name": name, "dorm": dorm })
-        self.added.append(id)
+        if self.ids
+            id = self.ids.pop()
+            name = "'" + random.choice(self.names) + " " + random.choice(self.names) + "'"
+            dorm = random.choice(self.dorms)
+            self.client.post("/students/register", {"id": id, "name": name, "dorm": dorm })
+            self.added.append(id)
 
     @task
     def delete(self):
-        if len(list):
-            id = self.added.pop()
-            self.client.delete(f"/students/delete/{id}")
-            self.ids.append(id)
+        if self.added:
+            self.client.delete(f"/students/delete/{self.added[-1]}")
+            ids.insert(0, added.pop())
+
+    @task
+    def update(self):
+        if self.added:
+            choice = random.getrandbits(1)
+            id = self.added[-1]
+
+            first = random.choice(self.names).strip("\n")
+            last = random.choice(self.names).strip("\n")
+            new = "'" + first + " " + last + "'"
+
+            if choice:
+                new = random.choice(self.dorms)
+            self.client.put(f"/students/update/{id}", {"changeDorm": choice, "new": new})
+            added.insert(0, added.pop())
+
 
     def on_stop(self):
         self.client.delete("/students/clear")
@@ -38,7 +54,6 @@ class MyTaskSet(TaskSet):
                         "'Mudd'", "'Blaisdell'", "'Sontag'", "'Dialynas'",
                         "'Walker'", "'Clark I'", "'Clark III'"]
         self.added = []
-
 
 
 class MyLocust(HttpLocust):
